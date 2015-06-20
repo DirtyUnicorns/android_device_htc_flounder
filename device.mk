ed103eb5c0fd510f702934968aafcbad4d574332
Josh Chasky <jchasky@gmail.com>
2015-06-20 09:35:40 -0400

    Flounder: DU Updater change
    Change-Id: Ibdf88703f4cffba106633df76a40d38c404332bf

--- device.mk
+++ device.mk
@@ -200,7 +200,8 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.bt.bdaddr_path=/sys/module/flounder_bdaddress/parameters/bdaddress \
     ro.frp.pst=/dev/block/platform/sdhci-tegra.3/by-name/PST \
     ro.ril.def.agps.mode=1 \
-    persist.tegra.compositor=glcomposer
+    persist.tegra.compositor=glcomposer \
+    ro.du.updater=flounder
 
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
