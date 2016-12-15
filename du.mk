09b056c356b88fb7af3e6fd5ae364c4d65a08a36
Alex Cruz <mazdarider23@gmail.com>
2016-12-15 18:04:35 +0100

    Remove nfc_enhanced.mk
    Change-Id: I8b321adfd931f297355df756895e338721af8949

--- du.mk
+++ du.mk
@@ -27,7 +27,6 @@ $(call inherit-product, device/htc/flounder/full_flounder.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
-$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 PRODUCT_NAME := du_flounder
 PRODUCT_DEVICE := flounder
